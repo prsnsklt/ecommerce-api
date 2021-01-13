97f94631a7ba835c50f834d5ed99d8acf72fb96e
Parasian <simanungkalitparasian@gmail.com>
2021-01-13 23:52:22 +0700

    Destroy Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,7 +90,7 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         $request['detail'] = $request->detail;
-        
+
         $product->update($request->all());
     }
 
@@ -102,6 +102,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        
+        $product->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
