195985a9c034714cd71f31c9b45b8847e47f6bee
Parasian <simanungkalitparasian@gmail.com>
2021-01-13 19:04:28 +0700

    Database Seeded with Faker, DB Added

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,8 +5,13 @@
 use App\Model\Product;
 use Faker\Generator as Faker;
 
+//bikin dummy data
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween($min = 100, $max = 1000),
+        'stock'=>$faker->randomDigit,
+        'discount'=>$faker->numberBetween($min= 10, $max=50)
     ];
 });
