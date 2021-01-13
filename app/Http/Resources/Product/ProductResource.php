2d996d990b4b3db303bd6dd3783f5dc8d37334d1
Parasian <simanungkalitparasian@gmail.com>
2021-01-13 22:06:49 +0700

    Product Resource Collection created

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,16 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock == 0 ? 'Out of stock!' : $this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=>round((1-($this->discount/100)) * $this->price,2),
+            'rating'=> $this->reviews->count() > 0 ?  //cek apakah ada rating pada product yang ditunjuk, kalau ada ubah hitungan ratingnya, kalau ga ada tampilkan 'No Rating yet'
+                    round($this->reviews->sum('star')/
+                    $this->reviews->count())
+                    : 'No Rating yet!',
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id) 
+                ]
 
         ];
     }
